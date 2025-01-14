e4bce5a0632b12e2576d8b1a0cff044a584eb6fb
kiyo8jo <hoxq4387@yahoo.co.jp>
2025-01-14 14:18:57 +0900

    headerデザイン変更・レスポンシブ対応

--- src/app/components/layouts/header/Header.tsx
+++ src/app/components/layouts/header/Header.tsx
@@ -1,7 +1,11 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
+import { FaHome } from "react-icons/fa";
+import { IoHomeOutline } from "react-icons/io5";
+import { FaPerson } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
 
 const Header = () => {
   return (
@@ -13,16 +17,28 @@ const Header = () => {
       </div>
       <div className={styles.link_container}>
         <div>
-          <Link href="/rent">借りる</Link>
+          <Link href="/rent">
+            <IoHomeOutline />
+            <p>借りる</p>
+          </Link>
         </div>
         <div>
-          <Link href="/buy">買う</Link>
+          <Link href="/buy">
+            <FaHome />
+            <p>買う</p>
+          </Link>
         </div>
         <div>
-          <Link href="/owner">オーナー</Link>
+          <Link href="/owner">
+            <FaPerson />
+            <p>オーナー</p>
+          </Link>
         </div>
         <div>
-          <Link href="/contact">問い合わせ</Link>
+          <Link href="/contact">
+            <IoMdMail />
+            <p>問い合わせ</p>
+          </Link>
         </div>
       </div>
     </div>
