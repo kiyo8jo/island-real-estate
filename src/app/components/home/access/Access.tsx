f078f7ced902036aa1bd855dfdf377ba673dea94
kiyo8jo <hoxq4387@yahoo.co.jp>
2025-01-14 21:12:06 +0900

    Accessコンポーネントのデザイン変更

--- src/app/components/home/access/Access.tsx
+++ src/app/components/home/access/Access.tsx
@@ -5,16 +5,16 @@ const Access = () => {
   return (
     <div className={styles.wrapper}>
       <Title title={"アクセス / Access"} />
-      <div className={styles.address_container}>
+      <div className={styles.content_container}>
         <div className={styles.left_container}>map</div>
         <div className={styles.right_container}>
-          <div className={styles.value_container}>
-            <p className={styles.label}>Address</p>
-            <p>aaaaaaaaa</p>
+          <div className={styles.container}>
+            <p className={styles.label}>住所</p>
+            <p className={styles.value}>東京都八丈島八丈町三根5392</p>
           </div>
-          <div className={styles.value_container}>
+          <div className={styles.container}>
             <p className={styles.label}>TEL</p>
-            <p>aaaaaaaaa</p>
+            <p className={styles.value}>00-0000-0000</p>
           </div>
         </div>
       </div>
