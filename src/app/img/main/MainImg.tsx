31ddf869355da61e11dc5d5f61efbb2f4ee9fe43
kiyo8jo <hoxq4387@yahoo.co.jp>
2025-01-19 21:13:34 +0900

    MainImgのクリッカブルマップをレスポンシブ対応

--- src/app/img/main/MainImg.tsx
+++ src/app/img/main/MainImg.tsx
@@ -1,8 +1,111 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./MainImg.module.css";
 import Link from "next/link";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+interface CoordsType {
+  mitsune: {
+    rent: number[];
+    buy: number[];
+  };
+  okago: {
+    rent: number[];
+    buy: number[];
+  };
+  kashitate: {
+    rent: number[];
+    buy: number[];
+  };
+  nakanogo: {
+    rent: number[];
+    buy: number[];
+  };
+  sueyoshi: {
+    rent: number[];
+    buy: number[];
+  };
+}
 
 const MainImg = () => {
+  const initialCoords: CoordsType = {
+    mitsune: {
+      rent: [465, 160, 510, 210],
+      buy: [518, 160, 557, 210],
+    },
+    okago: {
+      rent: [254, 250, 302, 300],
+      buy: [311, 250, 343, 300],
+    },
+    kashitate: {
+      rent: [373, 370, 421, 420],
+      buy: [430, 370, 464, 420],
+    },
+    nakanogo: {
+      rent: [558, 370, 602, 420],
+      buy: [617, 370, 648, 420],
+    },
+    sueyoshi: {
+      rent: [680, 250, 728, 300],
+      buy: [736, 250, 784, 300],
+    },
+  };
+  const [coords, setCoords] = useState<CoordsType>(initialCoords);
+
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  const updateCoordinates = useCallback(() => {
+    if (!imgRef.current) return;
+    const img = imgRef.current;
+    const widthRatio = img.clientWidth / 924;
+    const heightRatio = img.clientHeight / 500;
+    const calculateCoords = (coords: number[]) => {
+      return coords.map((coord, index: number) =>
+        index % 2 === 0 ? coord * widthRatio : coord * heightRatio
+      );
+    };
+    setCoords({
+      mitsune: {
+        rent: calculateCoords(initialCoords.mitsune.rent),
+        buy: calculateCoords(initialCoords.mitsune.buy),
+      },
+      okago: {
+        rent: calculateCoords(initialCoords.okago.rent),
+        buy: calculateCoords(initialCoords.okago.buy),
+      },
+      kashitate: {
+        rent: calculateCoords(initialCoords.kashitate.rent),
+        buy: calculateCoords(initialCoords.kashitate.buy),
+      },
+      nakanogo: {
+        rent: calculateCoords(initialCoords.nakanogo.rent),
+        buy: calculateCoords(initialCoords.nakanogo.buy),
+      },
+      sueyoshi: {
+        rent: calculateCoords(initialCoords.sueyoshi.rent),
+        buy: calculateCoords(initialCoords.sueyoshi.buy),
+      },
+    });
+  }, []);
+  useEffect(() => {
+    window.addEventListener("resize", updateCoordinates);
+    const handleLoad = () => {
+      updateCoordinates();
+    };
+    const img = imgRef.current;
+    if (img) {
+      img.addEventListener("load", handleLoad);
+    }
+    updateCoordinates();
+    return () => {
+      window.removeEventListener("resize", updateCoordinates);
+      if (img) {
+        img.removeEventListener("load", handleLoad);
+      }
+    };
+  }, [updateCoordinates]);
+
   return (
     <div className={styles.wrapper}>
       <Image
@@ -11,8 +114,9 @@ const MainImg = () => {
         useMap="#image-map"
         alt="main"
         fill
-        style={{ objectFit: "contain" }}
         priority
+        sizes="100vw"
+        ref={imgRef}
       />
       <map name="image-map">
         <Link href="/rent/mitsune">
@@ -20,7 +124,7 @@ const MainImg = () => {
             target="_blank"
             alt="mitusne_rent"
             title="mitusne_rent"
-            coords="465,169,510,192"
+            coords={coords.mitsune.rent.join(",")}
             shape="rect"
           />
         </Link>
@@ -30,7 +134,7 @@ const MainImg = () => {
             alt="mitusne_buy"
             title="mitusne_buy"
             href="http://localhost:3000/buy"
-            coords="518,167,557,192"
+            coords={coords.mitsune.buy.join(",")}
             shape="rect"
           />
         </Link>
@@ -40,7 +144,7 @@ const MainImg = () => {
             alt="okago_rent"
             title="okago_rent"
             href="http://localhost:3000/rent"
-            coords="254,271,302,294"
+            coords={coords.okago.rent.join(",")}
             shape="rect"
           />
         </Link>
@@ -50,7 +154,7 @@ const MainImg = () => {
             alt="okago_buy"
             title="okago_buy"
             href="http://localhost:3000/buy"
-            coords="311,271,343,294"
+            coords={coords.okago.buy.join(",")}
             shape="rect"
           />
         </Link>
@@ -60,7 +164,7 @@ const MainImg = () => {
             alt="kashitate_rent"
             title="kashitate_rent"
             href="http://localhost:3000/rent"
-            coords="373,399,421,421"
+            coords={coords.kashitate.rent.join(",")}
             shape="rect"
           />
         </Link>
@@ -70,7 +174,7 @@ const MainImg = () => {
             alt="kashitate_buy"
             title="kashitate_buy"
             href="http://localhost:3000/buy"
-            coords="430,398,464,420"
+            coords={coords.kashitate.buy.join(",")}
             shape="rect"
           />
         </Link>
@@ -80,7 +184,7 @@ const MainImg = () => {
             alt="nakanogo_rent"
             title="nakanogo_rent"
             href="http://localhost:3000/rent"
-            coords="558,397,602,420"
+            coords={coords.nakanogo.rent.join(",")}
             shape="rect"
           />
         </Link>
@@ -90,7 +194,7 @@ const MainImg = () => {
             alt="nakanogo_buy"
             title="nakanogo_buy"
             href="http://localhost:3000/buy"
-            coords="617,397,648,419"
+            coords={coords.nakanogo.buy.join(",")}
             shape="rect"
           />
         </Link>
@@ -100,7 +204,7 @@ const MainImg = () => {
             alt="sueyoshi_rent"
             title="sueyoshi_rent"
             href="http://localhost:3000/rent"
-            coords="680,267,728,294"
+            coords={coords.sueyoshi.rent.join(",")}
             shape="rect"
           />
         </Link>
@@ -110,7 +214,7 @@ const MainImg = () => {
             alt="sueyoshi_buy"
             title="sueyoshi_buy"
             href="http://localhost:3000/buy"
-            coords="736,268,770,293"
+            coords={coords.sueyoshi.buy.join(",")}
             shape="rect"
           />
         </Link>
